a3ff52745a6e787e4621917dff5cb34b39a15069
“JaredMathis” <wordslikejared@gmail.com>
2021-12-09 21:17:45 -0500

    2021-12-10T02:17:45.202Z

--- bible_public/bible_public_deploy.js
+++ bible_public/bible_public_deploy.js
@@ -4,6 +4,5 @@ import command_line_and_log from '../core/command_line_and_log.js';
 import git_acp from '../core/git_acp.js';
 let directory_asv = './public/asv';
 directory_add_ls_txt(directory_asv);
-directory_files_to_dictionary(directory_asv, [], ['.txt']);
 git_acp();
 command_line_and_log('firebase deploy');
