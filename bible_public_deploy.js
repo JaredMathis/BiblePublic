25bf6622a6be85526255ad2091625fe4a1e2389a
“JaredMathis” <wordslikejared@gmail.com>
2021-12-11 13:34:31 -0500

    changes

---
+++ bible_public_deploy.js
@@ -0,0 +1,8 @@
+import directory_files_to_dictionary from './core/directory_files_to_dictionary.js';
+import directory_add_ls_txt from './core/directory_add_ls_txt.js';
+import command_line_and_log from './core/command_line_and_log.js';
+import git_acp from './core/git_acp.js';
+let directory_asv = './public/asv';
+directory_add_ls_txt(directory_asv);
+git_acp();
+command_line_and_log('firebase deploy');
