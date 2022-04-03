6f7df07ad257c96ba9d015a7c2f9a939e3dfd54a
JaredMathis <jared.mathis@native.consulting>
2022-04-03 19:13:24 -0400

    update

--- bible_public/bible_cpdv_file_parse.js
+++ bible_public/bible_cpdv_file_parse.js
@@ -9,21 +9,22 @@ import list_where from '../foundation/list_where.js';
 import string_split from '../foundation/string_split.js';
 import bible_verse_data from '../bible/bible_verse_data.js';
 import is_string_not_empty from '../foundation/is_string_not_empty.js';
+import keys_unsorted from '../foundation/keys_unsorted.js';
 let version = 'cpdv';
 let directory_firebase_deploy = directory_firebase_deploy_get();
 let result = [];
 let bible = file_json_read('./public/cpdv/EntireBible-CPDV.json');
-let bible_keys = keys(bible);
+let bible_keys = keys_unsorted(bible);
 for_each(bible_keys, book_index => {
   if (book_index === 'charset') {
     return;
   }
   let book = bible[book_index];
-  let chapters = keys(book);
+  let chapters = keys_unsorted(book);
   // console.log({book_index,chapters});
   for_each(chapters, chapter_index => {
     let chapter = book[chapter_index];
-    let verses = keys(chapter);
+    let verses = keys_unsorted(chapter);
     for_each(verses, verse => {
       let text = chapter[verse];
       let tokens = string_split(text, ' ');
