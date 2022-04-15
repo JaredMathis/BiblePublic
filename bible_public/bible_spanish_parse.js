7b9a327d38fad9c3618d3f32bfdbfd3113c0aaa8
JaredMathis <jared.mathis@native.consulting>
2022-04-15 19:30:28 -0400

    progress

--- bible_public/bible_spanish_parse.js
+++ bible_public/bible_spanish_parse.js
@@ -1,3 +1,4 @@
+import file_read from "../core/file_read.js";
 import for_each from "../foundation/for_each.js";
 import range from "../foundation/range.js";
 
@@ -7,3 +8,13 @@ for_each(range(count), n => {
     let book_number = (n + 1).toString().padStart(2, '0');
     console.log(book_number);
 })
+
+let book_number = '01';
+let chapter = '1';
+
+let path = `./public/spanish/${book_number}/${chapter}.htm`;
+let text = file_read(path);
+
+
+
+console.log(text);
