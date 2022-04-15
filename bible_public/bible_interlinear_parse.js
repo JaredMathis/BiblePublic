21d888026ec6e673a24cc45f7b945014aa060a09
JaredMathis <jared.mathis@native.consulting>
2022-04-15 16:30:30 -0400

    progress

--- bible_public/bible_interlinear_parse.js
+++ bible_public/bible_interlinear_parse.js
@@ -1,6 +1,7 @@
 import file_read from "../core/file_read.js";
 import list_map from "../foundation/list_map.js";
 import xml2js from 'xml2js';
+import assert from "../foundation/assert.js";
 
 
 let xml = file_read('./public/interlinear/berean/berean-interlinear-glosses.xml');
@@ -8,6 +9,10 @@ let xml = file_read('./public/interlinear/berean/berean-interlinear-glosses.xml'
 let parsed = await xml2js.parseStringPromise(xml)
 
 for (let verse of parsed.root.verse) {
-    let first = verse[0].w[0]
-    console.log(parsed.root.verse[0].w[0])
+    console.log({verse})
+    let first = verse.w[0]
+    let split1 = first.$.osisId.split('!');
+    assert(split1.length === 2);
+    let i = split1[1] - 1;
+    console.log(parsed.root.verse.w[0])
 }
