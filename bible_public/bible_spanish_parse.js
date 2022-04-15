911815101ec617062bbfc287808017b4765e15a6
JaredMathis <jared.mathis@native.consulting>
2022-04-15 19:20:05 -0400

    refactor

--- bible_public/bible_spanish_parse.js
+++ bible_public/bible_spanish_parse.js
@@ -4,5 +4,6 @@ import range from "../foundation/range.js";
 let count = 66;
 
 for_each(range(count), n => {
-    console.log((n + 1).toString().padStart(2, '0'));
+    let book_number = (n + 1).toString().padStart(2, '0');
+    console.log(book_number);
 })
