4903f12332abe2b078df285ab55dba5c77e0725f
JaredMathis <jared.mathis@native.consulting>
2022-05-08 19:58:20 -0400

    progress

--- bible_public/bible_drv_filter.js
+++ bible_public/bible_drv_filter.js
@@ -2,5 +2,5 @@ import file_read from "../core/file_read.js";
 
 let text = file_read('./public/drv/pg1581.txt');
 let lines = text.split('\n').map(line => line.trim());
-let filtered = lines.filter(line => line.match(/.* Chapter \d{1,3}$/g))
+let filtered = lines.filter(line => line.match(/.* Chapter \d{1,3}$/g) || line.match(/\d{1,3}:\d{1,3}\. /))
 console.log(filtered)
